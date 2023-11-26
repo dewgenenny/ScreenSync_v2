80039016786a44272d2de4ba22314c761d888ba6
dewgenenny <tom@penberth.com>
2023-11-26 21:57:46 +0100

    Add HSV calculations. Change color update process to use DP27 color setting (music sync mode) to improve latency.

--- screen-sync/screen_sync/bulb_control/tuya_bulb.py
+++ screen-sync/screen_sync/bulb_control/tuya_bulb.py
@@ -1,7 +1,18 @@
 import tinytuya
 from .abstract_bulb_control import AbstractBulbControl
 
+def generate_dp27_string(self, hue, saturation, value, mode='gradient'):
+    """Generate the DP27 string manually with given hue, saturation, and value."""
+    h_hex = '{:04x}'.format(hue)
+    s_hex = '{:04x}'.format(saturation)
+    v_hex = '{:04x}'.format(value)
+
+    mode_flag = '1' if mode == 'gradient' else '0'
+    dp27_string = f"0{h_hex}{s_hex}{v_hex}00000000"  # Additional brightness/color temp set to max
+    return dp27_string
+
 class TuyaBulbControl(AbstractBulbControl):
+
     def __init__(self, device_id, local_key, ip):
         self.device_id = device_id
         self.local_key = local_key
@@ -11,10 +22,16 @@ class TuyaBulbControl(AbstractBulbControl):
     def connect(self):
         self.bulb = tinytuya.BulbDevice(self.device_id, self.ip, self.local_key, persist=True)
         self.bulb.set_version(3.3)
+        self.bulb.set_socketRetryLimit(1)
+        self.bulb.set_socketTimeout(1)
+        self.bulb.set_retry(retry=False)
+        self.bulb.set_socketPersistent(True)
 
-    def set_color(self, r, g, b):
+
+    def set_color(self, h,s,v):
+        dp27_string = generate_dp27_string(self,h,s,v)
         if self.bulb:
-            self.bulb.set_colour(r, g, b)
+            self.bulb.set_multiple_values( {'21': 'music', '27': dp27_string}, nowait=True)
 
     def turn_off(self):
         if self.bulb:
@@ -22,4 +39,9 @@ class TuyaBulbControl(AbstractBulbControl):
 
     def turn_on(self):
         if self.bulb:
+            self.bulb.set_mode(mode='white',nowait=True)
             self.bulb.turn_on()
+
+    def status(self):
+        if self.bulb:
+            return self.bulb.status()
--- screen-sync/screen_sync/color_processing.py
+++ screen-sync/screen_sync/color_processing.py
@@ -1,13 +1,19 @@
 from PIL import ImageGrab, ImageStat, Image, ImageEnhance, ImageColor
+import colorsys
 
+
+# Global variable to cache screen size
+screen_width, screen_height = None, None
 
 def get_screen_center(size=(100, 100)):
-    """Calculate the coordinates for a central region of the screen."""
-    screen = ImageGrab.grab()
-    screen_width, screen_height = screen.size
+    global screen_width, screen_height
+    if not screen_width or not screen_height:
+        screen = ImageGrab.grab()
+        screen_width, screen_height = screen.size
     center_x, center_y = screen_width // 2, screen_height // 2
     half_width, half_height = size[0] // 2, size[1] // 2
     return center_x - half_width, center_y - half_height, center_x + half_width, center_y + half_height
+
 
 def capture_screen_center():
     """Capture a 100x100 pixel box at the center of the screen."""
@@ -34,9 +40,19 @@ def adjust_color(r, g, b, saturation_factor=1.5):
 
     return enhanced_color
 
-def process_screen_color():
-    """Capture the screen color and process it."""
-    screenshot = capture_screen_center()
+def process_screen_color(size=(100, 100)):
+    """Capture the screen color and process it, returning HSV values."""
+    bbox = get_screen_center(size)
+    screenshot = ImageGrab.grab(bbox=bbox)
     r, g, b = get_average_color(screenshot)
-    adjusted_r, adjusted_g, adjusted_b = adjust_color(r, g, b, saturation_factor=3.0)
-    return adjusted_r, adjusted_g, adjusted_b
+    adjusted_r, adjusted_g, adjusted_b = adjust_color(r, g, b, saturation_factor=3)
+
+    # Convert RGB to HSV
+    h, s, v = colorsys.rgb_to_hsv(adjusted_r / 255.0, adjusted_g / 255.0, adjusted_b / 255.0)
+
+    # Scale HSV values to the appropriate range
+    h_scaled = int(h * 360)  # Hue: 0-360
+    s_scaled = int(s * 1000) # Saturation: 0-1000
+    v_scaled = int(v * 1000) # Value: 0-1000
+
+    return h_scaled, s_scaled, v_scaled
